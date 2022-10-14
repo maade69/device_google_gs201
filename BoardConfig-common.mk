ed8cf3afec9ea527504b5bf7f5115de0e8e512a8
Vladislav <stvlad06@gmail.com>
2022-10-14 21:46:21 +0300

    move back to armv8-a as 2nd arch
    build/make/core/combo/TARGET_linux-arm.mk:53: error: Incorrect TARGET_2ND_ARCH_VARIANT, armv8-2a. Use armv8-a instead..
    
    Change-Id: Iea86a590f8cb582964c0e820766af6f47bda2931

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -40,7 +40,7 @@ FORCE_MULTILIB_FIRST_ON_DEVICE := true
 
 # Build the 32 bit targets
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
